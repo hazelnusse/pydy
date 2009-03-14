ba162ebc8376a0193aafb5caf5379f26d5edfd94
Luke Peterson <hazelnusse@gmail.com>
2009-03-13 17:24:06 -0700

    Tests for UnitVector written

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,4 +1,4 @@
-from PyDy import ReferenceFrame, dot, cross
+from PyDy import ReferenceFrame, dot, cross, UnitVector
 from sympy import symbols
 import numpy as N
 e1 = N.array([1.0,0.0,0.0])
@@ -16,25 +16,28 @@ def vec_eq(a, b, eps=1e-8):
     return N.dot((a.v['num'] - b),(a.v['num'] - b)) < eps
 
 def test_UnitVector():
-	A = ReferenceFrame('A')
-	assert eq(dot(A[1], A[1]), 1.0)
-	assert eq(dot(A[1], A[2]), 0.0)
-	assert eq(dot(A[1], A[3]), 0.0)
-	assert eq(dot(A[2], A[1]), 0.0)
-	assert eq(dot(A[2], A[2]), 1.0)
-	assert eq(dot(A[2], A[3]), 0.0)
-	assert eq(dot(A[3], A[1]), 0.0)
-	assert eq(dot(A[3], A[2]), 0.0)
-	assert eq(dot(A[3], A[3]), 1.0)
-        assert vec_eq(cross(A[1], A[1]), zero)
-        assert vec_eq(cross(A[1], A[2]), e3)
-        assert vec_eq(cross(A[1], A[3]), e2n)
-        assert vec_eq(cross(A[2], A[1]), e3n)
-        assert vec_eq(cross(A[2], A[2]), zero)
-        assert vec_eq(cross(A[2], A[3]), e1)
-        assert vec_eq(cross(A[3], A[1]), e2)
-        assert vec_eq(cross(A[3], A[2]), e1n)
-        assert vec_eq(cross(A[3], A[3]), zero)
+    a = UnitVector("A", 3)
+
+def test_dot_cross():
+    A = ReferenceFrame('A')
+    assert eq(dot(A[1], A[1]), 1.0)
+    assert eq(dot(A[1], A[2]), 0.0)
+    assert eq(dot(A[1], A[3]), 0.0)
+    assert eq(dot(A[2], A[1]), 0.0)
+    assert eq(dot(A[2], A[2]), 1.0)
+    assert eq(dot(A[2], A[3]), 0.0)
+    assert eq(dot(A[3], A[1]), 0.0)
+    assert eq(dot(A[3], A[2]), 0.0)
+    assert eq(dot(A[3], A[3]), 1.0)
+    assert vec_eq(cross(A[1], A[1]), zero)
+    assert vec_eq(cross(A[1], A[2]), e3)
+    assert vec_eq(cross(A[1], A[3]), e2n)
+    assert vec_eq(cross(A[2], A[1]), e3n)
+    assert vec_eq(cross(A[2], A[2]), zero)
+    assert vec_eq(cross(A[2], A[3]), e1)
+    assert vec_eq(cross(A[3], A[1]), e2)
+    assert vec_eq(cross(A[3], A[2]), e1n)
+    assert vec_eq(cross(A[3], A[3]), zero)
 
 def test_expressions():        
 	x, y = symbols("x y")
