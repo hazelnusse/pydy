43134854fd53653d6c04632f08a866ef119ccd45
Luke Peterson <hazelnusse@gmail.com>
2009-08-09 12:49:57 -0700

    interface stuff

--- alwrapper.py
+++ alwrapper.py
@@ -35,6 +35,8 @@ class Al(object):
         al_output = self._child.before
         s = re.split("(-> \(\d+\))", al_output)
         input = s[0]
+        #print input
+        #print command
         assert input.strip() == command.strip()
         outputs = s[1:]
         pairs = []
@@ -45,12 +47,12 @@ class Al(object):
             pairs.append((prompt, output))
         return pairs
 
+s = "(-c3*s1 - c1*s2*s3)/((c1*c2 + c2*s1*(c3*s1 + c1*s2*s3)/(c1*c3 - s1*s2*s3))*(c1*c3 - s1*s2*s3))"
 
+s2 = s.replace("**", "^")
+#print s2
 a = Al()
-print a.run_command("variables a,b,c")
-print a.run_command('test = (a+b+c)^2')
-print a.run_command('test2 = expand(test, 1:3)')
-print a.run_command("variables a")
-print a.run_command('AUTOZ ON')
-print a.run_command('frames D, E')
-print a.run_command('simprot(E, D, 1, a)')
+print a.run_command("variables a,b,c, s1, s2, s3, c1, c2, c3, u1, u2, u3, u4, u5, u6")
+#print a.run_command('test = (a+b)^10')
+print a.run_command('test = %s' % s2)
+print a.run_command('test2 = expand(test)')
