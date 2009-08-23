a9b8312300f500c8ae8441f173db32c4cf58ed3c
Luke Peterson <hazelnusse@gmail.com>
2009-08-23 12:42:35 -0700

    Updated simplependulum example to include damping

--- examples/pendulum_eoms.py
+++ examples/pendulum_eoms.py
@@ -1,21 +1,21 @@
-# Sun Aug 23 12:04:50 2009
+# Sun Aug 23 12:39:43 2009
 from numpy import sin, cos, tan, vectorize
 
 def f(x, t, parameter_list):
     # Unpacking the parameters
-    m, g, l = parameter_list
+    m, g, l, b = parameter_list
     # Unpacking the states (q's and u's)
     q1, u1 = x
     s1 = sin(q1)
     # Kinematic differential equations
     q1p = u1
     # Dynamic differential equations
-    u1p = -g*s1/l
+    u1p = -g*s1/l - b*u1/(l**2*m)
     return [q1p, u1p]
 
 def qdot2u(q, qd, parameter_list):
     # Unpacking the parameters
-    m, g, l = parameter_list
+    m, g, l, b = parameter_list
     # Unpacking the q's and qdots
     q1 = q
     q1p = qd
@@ -26,14 +26,14 @@ def qdot2u(q, qd, parameter_list):
 
 def animate(q, parameter_list):
     # Unpacking the parameters
-    m, g, l = parameter_list
+    m, g, l, b = parameter_list
     # Unpacking the coordinates
     q1 = q
     # Trigonometric functions needed
     s1 = sin(q1)
     c1 = cos(q1)
     # Position of Points and Axis/Angle Calculations
-    A3_1 = s1
-    A3_2 = 0
+    A3_1 = 0
+    A3_2 = -s1
     A3_3 = c1
     return [A3_1, A3_2, A3_3, 0]
--- examples/simplependulum.py
+++ examples/simplependulum.py
@@ -4,17 +4,17 @@ from pydy import *
 N = NewtonianReferenceFrame('N')
 
 # Declare parameters, coordinates, speeds
-m, g, l = N.declare_parameters('m g l')
+m, g, l, b = N.declare_parameters('m g l b')
 (q1,), q_list, qdot_list = N.declare_coords('q', 1)
 (u1,), u_list, udot_list = N.declare_speeds('u', 1)
 
 # Orient the A reference frame
-A = N.rotate('A', 2, q1)
+A = N.rotate('A', 1, q1)
 # Define the position from the hinge
-P = N.O.locate('P', l*A[3], mass=m)
+P = N.O.locate('P', -l*A[3], mass=m)
 
 # Define the generalized speed
-u_defs = N.define_speeds([Eq(u1, dot(A.ang_vel(), A[2]))])
+u_defs = N.define_speeds([Eq(u1, dot(A.ang_vel(), A[1]))])
 
 # Compute the kinematic differential equations
 T, Tinv, kindiffs = N.form_kindiffs(u_defs, qdot_list)
@@ -25,20 +25,23 @@ for qd in qdot_list:
 
 # Set the angular velocity and velocities to only involve expressions involving
 # the generalized speeds
-A._wrel = Vector(u1*A[2])
+A._wrel = Vector(u1*A[1])
 P._vrel = cross(A.ang_vel(), P.rel(N.O))
 
 # Set the kinematic differential equations
 N.setkindiffs(kindiffs)
 
 # Apply gravity
-N.gravity(g*N[3])
+N.gravity(-g*N[3])
+# Apply a friction torque at hinge
+A.apply_torque(-b*u1*A[1], N)
 
 # Form Kane's equations and solve them for the udots
 kanes_eqns = N.form_kanes_equations()
 dyndiffs = N.solve_kanes_equations()
 print 'Dynamic differential equations'
 for ud in udot_list:
+    dyndiffs[ud] = dyndiffs[ud].expand()
     print ud, '=', dyndiffs[ud]
 
 N.setdyndiffs(dyndiffs)
