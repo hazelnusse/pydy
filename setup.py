1c7dcb703c68e60aa0b86564d8e17d75fb0caa1d
Ondrej Certik <ondrej@certik.cz>
2010-04-25 15:41:27 -0700

    setup.py file added
    Also the pydy package was created and the pydy.py was moved into it.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "pydy",
+    version = "0.1",
+    packages = ['pydy'],
+    author = "Dale Peterson",
+    author_email = "",
+    description = "",
+    license = "BSD",
+)
